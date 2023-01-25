1ee2719a023d118be9be0925094a647da43a063d
PolinaAleksandrova <polina.aleksandrova@nure.ua>
2023-01-26 00:24:28 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import Header from "../components/Header/Header";
+
+import Courses from "../components/Courses-section/Courses";
+import FreeCourse from "../components/Free-course-section/FreeCourse";
+
+import Footer from "../components/Footer/Footer";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <Header />
+
+      <Courses />
+      <FreeCourse />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Home;
